85f5bc80c56fee85101b57b6a9a8595261cc9b6c
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 05:18:13 -0700

    shell.c minor fix

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ int main(void)
 
 	while (1)
         {
-                printf("c is fun");
+                printf("cisfun");
                 commandLength = getline(&command, &bufferSize, stdin);
                 if (commandLength == -1)
                 {
