0d3d4789d3bbbdbb1ad6256e4560cf8750e0ae38
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 04:51:17 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -10,6 +10,23 @@ int main(void)
 	size_t bufferSize = 0;
 	ssize_t commandLength;
 
+	while (1)
+        {
+                printf("Shell-> ");
+                commandLength = getline(&command, &bufferSize, stdin);
+                if (commandLength == -1)
+                {
+                        printf("Error reading input\n");
+                        perror("command");
+                        free(command);
+                        exit(1);
+                }
+                command[strlen(command) - 1] = '\0';                if (strcmp(command, "exit") == 0)
+                {
+                        free(command);
+                        exit(0);
+                }
+
 	
 	executeCommand(command);
 
