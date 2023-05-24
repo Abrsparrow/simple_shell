f4cd10734e900d90195e060b6679ef7bb016e42b
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 09:12:36 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -6,49 +6,56 @@
  */
 int main(void)
 {
-	char *command = NULL;
-	size_t bufferSize = 0;
-	ssize_t commandLength;
+  char *command = NULL;
+  size_t bufferSize = 0;
+  ssize_t commandLength;
 
-	while (1)
-	{
-		printf("cisfun");
-		commandLength = getline(&command, &bufferSize, stdin);
-		if (commandLength == -1)
-		{
-			printf("Error reading input\n");
-			perror("command");
-			free(command);
-			exit(1);
-		}
-		command[strlen(command) - 1] = '\0';
-		if (strcmp(command, "exit") == 0)
-		{
-			free(command);
-			exit(0);
-		}
-		executeCommand(command);
-		{
-			printf("#cisfun$ ");
-			commandLength = getline(&command, &bufferSize, stdin);
-			if (commandLength == -1)
-			{
-				printf("Error reading input\n");
-				perror("command");
-				free(command);
-				exit(1);
-			}
-			command[strlen(command) - 1] = '\0';
-			if (strcmp(command, "exit") == 0)
-			{
-				free(command);
-				exit(0);
-			}
-			if (!isatty(fileno(stdin)))
-			{
-				break;
-			}
-		}
-		free(command);
-		return (0);
-	}
+  while (1)
+<<<<<<< HEAD
+  {
+    printf("cisfun");
+    commandLength = getline(&command, &bufferSize, stdin);
+    if (commandLength == -1)
+    {
+      printf("Error reading input\n");
+      perror("command");
+      free(command);
+      exit(1);
+    }
+    command[strlen(command) - 1] = '\0';
+    if (strcmp(command, "exit") == 0)
+    {
+      free(command);
+      exit(0);
+    }
+    executeCommand(command);
+=======
+        {
+                printf("#cisfun$ ");
+                commandLength = getline(&command, &bufferSize, stdin);
+                if (commandLength == -1)
+                {
+                        printf("Error reading input\n");
+                        perror("command");
+                        free(command);
+                        exit(1);
+                }
+                command[strlen(command) - 1] = '\0';
+
+                if (strcmp(command, "exit") == 0)
+                {
+                        free(command);
+                        exit(0);
+                }
+  
+  executeCommand(command);
+>>>>>>> 605dfacefb2b09afc7cb8c28399f47cbcc9b843e
+
+    if (!isatty(fileno(stdin)))
+    {
+      break;
+    }
+  }
+  free(command);
+  return (0);
+}
