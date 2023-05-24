65a83ffd9261898b1456c7b1b7c9d0c5808a0221
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 04:38:47 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H/* Standard library */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#endif
