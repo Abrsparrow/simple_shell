e637928ff21934bdf176c30f839004cd68938fdc
KillerA-z1 <zeoof@DESKTOP-VUTJ4PI.localdomain>
2023-05-24 14:59:08 +0300

    Execute the command using execvp

---
+++ execode.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * executeCommand - Execute the command using execvp
+ * @command: The command to execute
+ */
+
+void executeCommand(char *command)
+{
+	char **argv = parseCommand(command);
+
+	int pid = fork();
+
+	if (pid < 0)
+	{
+		printf("Fork error\n");
+		free(argv);
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		/* printf("Executing command: %s\n", command); */
+		execvp(argv[0], argv);
+
+		perror("Error executing command");
+		free(argv);
+		exit(1);
+	}
+	else
+	{
+		wait(NULL);
+	}
+
+	free(argv);
+}
+
+
+
+
+
+
+
