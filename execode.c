a770eecfbc3ed417ba16df66296617f77793f7bb
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 17:52:37 -0700

    hot fix 16

--- execode.c
+++ execode.c
@@ -7,27 +7,54 @@ void executeCommand(char *command)
 {
 	int pid = fork();
 
-	if (pid < 0)
-	{
-		perror("Fork error\n");
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		char *args[4];
-		char *envp[] = {NULL};
+    if (pid < 0)
+    {
+        perror("Fork error\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        char *args[2] = {command, NULL};
+        char *path = getenv("PATH");
+        char *token;
+        char *pathCommand;
+        int execResult;
 
-		args[0] = "/bin/sh";
-		args[1] = "-c";
-		args[2] = command;
-		args[3] = NULL;
+        if (path == NULL)
+        {
+            perror("Unable to get PATH\n");
+            exit(EXIT_FAILURE);
+        }
 
-		execve("/bin/sh", args, envp);
-		perror("/bin/sh");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(NULL);
-	}
+        token = strtok(path, ":");
+        while (token != NULL)
+        {
+            pathCommand = malloc(strlen(token) + strlen(command) + 2);
+            if (pathCommand == NULL)
+            {
+                perror("Malloc error\n");
+                exit(EXIT_FAILURE);
+            }
+            _strcpy(pathCommand, token);
+            strcat(pathCommand, "/");
+            strcat(pathCommand, command);
+
+            execResult = execve(pathCommand, args, environ);
+            free(pathCommand);
+
+            if (execResult != -1)
+            {
+                break;
+            }
+
+            token = strtok(NULL, ":");
+        }
+
+        perror(command);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(NULL);
+    }
 }
