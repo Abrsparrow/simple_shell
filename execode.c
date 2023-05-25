fdda4e573429858fa94abb980be95774601ee637
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-24 19:09:31 -0700

    hot fix 18

--- execode.c
+++ execode.c
@@ -5,34 +5,33 @@
  */
 void executeCommand(char *command)
 {
-    int pid = fork();
+	pid_t pid = fork();
 
-    if (pid < 0)
-    {
-        perror("Fork error\n");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        char **args = malloc(4 * sizeof(char *));
-        if (args == NULL)
-        {
-            perror("Memory allocation error\n");
-            exit(EXIT_FAILURE);
-        }
-        
-        args[0] = "/bin/sh";
-        args[1] = "-c";
-        args[2] = command;
-        args[3] = NULL;
+	if (pid < 0)
+	{
+		perror("Fork error\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char **args = malloc(4 * sizeof(char *));
 
-        /* char *envp[] = {NULL}; */
-        execve(args[0], args, environ);
-        perror(args[0]);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(NULL);
-    }
+		if (args == NULL)
+		{
+			perror("Memory allocation error\n");
+			exit(EXIT_FAILURE);
+		}
+		args[0] = "/bin/sh";
+		args[1] = "-c";
+		args[2] = command;
+		args[3] = NULL;
+		/* char *envp[] = {NULL}; */
+		execve(args[0], args, environ);
+		perror(args[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
 }
