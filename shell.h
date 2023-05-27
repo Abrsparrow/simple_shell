387359eefa6c283d92ec414ef3487bf88c71d911
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-05-26 18:34:31 -0700

    adding alias struct

--- shell.h
+++ shell.h
@@ -11,7 +11,22 @@
 /* define Types */
 extern char **environ;
 #define MAX_COMMAND_LENGTH 100
-
+#define MAX_ALIASES 100
+#define MAX_ALIAS_NAME 100
+#define MAX_ALIAS_VALUE 100
+/**
+ * struct Alias - Structure to store an alias mapping.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ *
+ * Description: This structure represents an alias mapping, which consists
+ * of a name and its corresponding value.
+ */
+typedef struct Alias
+	{
+	char name[MAX_ALIAS_NAME];
+	char value[MAX_ALIAS_VALUE];
+	} Alias;
 /* helper_function */
 int _strlen(const char *s);
 int _strcmp(const char *s1, const char *s2);
